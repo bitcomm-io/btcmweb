b36b50370b716338f550020bc2f0f89a256b1c72
Bitcomm <jeff@bitcomm.io>
2024-03-12 11:58:31 +0800

    commit

--- src/jsonrpc.rs
+++ src/jsonrpc.rs
@@ -1,12 +1,29 @@
 #[allow(unused_imports)]
 use axum::{
-    extract::{FromRequest, Json}, http::{self, StatusCode}, response::{Json as JsonResponse, Response}, Extension //routing::post, Router
+    extract::{ FromRequest, Json },
+    http::{ self, StatusCode },
+    response::{ Json as JsonResponse, Response },
+    Extension, //routing::post, Router
 };
 // use jsonwebtoken::{decode, Algorithm, DecodingKey, Validation};
 // use btcmtools::LOGGER;
 use serde::{ Deserialize, Serialize };
 // use slog::info;
-use std::collections::HashMap;
+use std::{ collections::HashMap, future::Future, pin::Pin };
+
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref FUNCTION_MAP: HashMap<&'static str, Box<dyn Fn(Json<JsonRequest>) 
+        -> Pin<Box<dyn Future<Output = JsonResponse<JsonResponseWrapper>> 
+            + Send + Sync>> + Send + Sync>> 
+        = {
+            let mut m = HashMap::new();
+            m.insert("add", returns_function_pointer(|json| Box::pin(add(json))));
+            // Mutex::new(m)
+            m
+        };
+}
 
 // use crate::webserver;
 
@@ -25,30 +42,47 @@ pub struct JsonResponseWrapper {
     id: serde_json::Value,
 }
 
+fn returns_function_pointer<F>(
+    f: F
+) -> Box<
+        dyn (Fn(
+            Json<JsonRequest>
+        ) -> Pin<Box<dyn Future<Output = JsonResponse<JsonResponseWrapper>> + Send + Sync>>) +
+            Send +
+            Sync
+    >
+    where
+        F: Fn(
+            Json<JsonRequest>
+        ) -> Pin<Box<dyn Future<Output = JsonResponse<JsonResponseWrapper>> + Send + Sync>> +
+            Send +
+            Sync +
+            'static
+{
+    Box::new(move |input| f(input))
+}
+
 pub async fn json_rpc_handler(req: Json<JsonRequest>) -> JsonResponse<JsonResponseWrapper> {
-    let response = match req.method.as_str() {
-        "add" => {
-            if let Some(params) = &req.params {
-                if let (Some(a), Some(b)) = (params.get("a"), params.get("b")) {
-                    if let (Some(a), Some(b)) = (a.as_i64(), b.as_i64()) {
-                        JsonResponseWrapper {
-                            jsonrpc: req.jsonrpc.clone(),
-                            result: serde_json::to_value(a + b).unwrap(),
-                            id: req.id.clone(),
-                        }
-                    } else {
-                        error_response(req.jsonrpc.clone(), req.id.clone())
-                    }
-                } else {
-                    error_response(req.jsonrpc.clone(), req.id.clone())
-                }
-            } else {
-                error_response(req.jsonrpc.clone(), req.id.clone())
+    if let Some(func_pointer) = FUNCTION_MAP.get(req.method.as_str()) {
+        func_pointer(req).await
+    } else {
+        JsonResponse(error_response(req.jsonrpc.clone(), req.id.clone()))
+    }
+}
+
+async fn add(req: Json<JsonRequest>) -> JsonResponse<JsonResponseWrapper> {
+    if let Some(params) = &req.params {
+        if let (Some(a), Some(b)) = (params.get("a"), params.get("b")) {
+            if let (Some(a), Some(b)) = (a.as_i64(), b.as_i64()) {
+                return JsonResponse(JsonResponseWrapper {
+                    jsonrpc: req.jsonrpc.clone(),
+                    result: serde_json::to_value(a + b).unwrap(),
+                    id: req.id.clone(),
+                });
             }
         }
-        _ => error_response(req.jsonrpc.clone(), req.id.clone()),
-    };
-    JsonResponse(response)
+    }
+    JsonResponse(error_response(req.jsonrpc.clone(), req.id.clone()))
 }
 
 fn error_response(jsonrpc: String, id: serde_json::Value) -> JsonResponseWrapper {
@@ -61,61 +95,3 @@ fn error_response(jsonrpc: String, id: serde_json::Value) -> JsonResponseWrapper
         id,
     }
 }
-
-// #[derive(Debug, Serialize, Deserialize)]
-// struct Claims {
-//     // ...您的声明字段...
-// }
-// use axum::extract::request_parts;
-
-// async fn jwt_auth_middleware<B>(
-//     mut req: RequestParts<B>,
-// ) -> Result<RequestParts<B>, Response> {
-//     if let Some(auth_header) = req.headers().get(http::header::AUTHORIZATION) {
-//         if let Ok(auth_str) = auth_header.to_str() {
-//             if auth_str.starts_with("Bearer ") {
-//                 let token = auth_str.trim_start_matches("Bearer ");
-//                 let validation = Validation::new(Algorithm::HS256);
-//                 let key = DecodingKey::from_secret("your-secret-key".as_ref());
-//                 match decode::<Claims>(token, &key, &validation) {
-//                     Ok(_) => return Ok(req),
-//                     Err(_) => (),
-//                 }
-//             }
-//         }
-//     }
-
-//     Err(Response::builder()
-//         .status(StatusCode::UNAUTHORIZED)
-//         .body("Invalid JWT token".into())
-//         .unwrap())
-// }
-
-// #[tokio::main]
-// async fn main() {
-//     let server_address = webserver::get_adminserver_port();
-//     let app = Router::new().route("/json-rpc", post(json_rpc_handler));
-
-//     let listener = tokio::net::TcpListener::bind(server_address.as_str()).await.unwrap();
-//     info!(LOGGER, "http listening on {}", listener.local_addr().unwrap());
-//     axum::serve(listener, app).await.unwrap();
-// }
-
-// // JWT验证中间件
-// pub async fn jwt_authenticate_handler(
-//     Extension(token): Extension<String>
-// ) -> Result<(), JsonResponse<serde_json::Value>> {
-//     // let token = req.into_inner().0.token;
-//     // 在这里验证 JWT token
-//     let secret = "your_secret";
-//     let key = DecodingKey::from_secret(secret.as_ref());
-//     match decode::<HashMap<String, String>>(&token, &key, &Validation::new(Algorithm::HS256)) {
-//         Ok(_) => Ok(()),
-//         Err(_) => Err(JsonResponse(serde_json::json!({
-//             "error": "Unauthorized"
-//         })))
-//     }
-// }
-
-
-
--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,35 @@ pub mod jsonrpc;
 // mod usermanager2;
 // mod usermanager3;
 // mod usermanager4;
+
+
+// macro_rules! create_func_map {
+//     ($map:ident, $($func:ident),*) => {{
+//         $( $map.insert(stringify!($func), $func as fn(Json<JsonRequest>) -> JsonResponse<JsonResponseWrapper>); )*
+//     }};
+// }
+
+// macro_rules! create_afunc_map {
+//     ($map:ident, $($func:ident),*) => {{
+//         $( $map.insert(stringify!($func), 
+//             std::sync::Arc::new(|request: Json<JsonRequest>| -> JsonResponse<JsonResponseWrapper> {
+//                 async move { $func(request).await }
+//             }) as std::sync::Arc<dyn Fn(Json<JsonRequest>) -> JsonResponse<JsonResponseWrapper> + Send + Sync>); )*
+//     }};
+// }
+
+// fn main() {
+//     let mut func_map = HashMap::new();
+//     create_func_map!(func_map, add, subtract);
+
+//     // 示例：从 HashMap 中调用函数
+//     if let Some(func) = func_map.get("add") {
+//         let result = func(10, 5);
+//         println!("Result of add function: {}", result);
+//     }
+
+//     if let Some(func) = func_map.get("subtract") {
+//         let result = func(10, 5);
+//         println!("Result of subtract function: {}", result);
+//     }
+// }
